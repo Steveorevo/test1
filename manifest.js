037d2e58f2a5822f3ac49391f9548842e993efdc
Stephen J. Carnam <steve@steveorevo.com>
2018-10-01 15:23:55 -0700

    Added manifest

---
+++ manifest.js
@@ -0,0 +1,17 @@
+/**
+ * This code is machine generated.
+ */
+if (typeof RED.publishflows == 'undefined') RED.publishflows = {};
+if (typeof RED.publishflows.manifest == 'undefined') RED.publishflows.manifest = [];
+RED.publishflows.manifests.push(
+  {
+    "tabs": [
+      {
+        "label": "Test 1 Flow",
+        "id": "5aa020b6.706c4",
+        "checked": "checked"
+      }
+    ],
+    "project": "test1"
+  }
+);
