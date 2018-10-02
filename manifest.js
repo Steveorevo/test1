3011dfa3639ff277f10d1f6dca1371331bbb4f45
Stephen J. Carnam <steve@steveorevo.com>
2018-10-01 22:48:41 -0700

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,21 +2,18 @@
  * This code is machine generated.
  */
 module.exports = function(RED) {
-   function test1() {
-     if (typeof RED.publishflows != 'undefined') {
-       RED.publishflows.manifests.push(
-         {
-           "tabs": [
-             {
-               "label": "Test 1 Flow",
-               "id": "5aa020b6.706c4",
-               "checked": "checked"
-             }
-           ],
-           "project": "test1"
-         }
-       );
-     }
-   }
-   RED.nodes.registerType("test1", test1);
+  if (typeof RED.publishflows != 'undefined') {
+    RED.publishflows.manifests.push(
+      {
+        "tabs": [
+          {
+            "label": "Test 1 Flow",
+            "id": "5aa020b6.706c4",
+            "checked": "checked"
+          }
+        ],
+        "project": "test1"
+      }
+    );
+  }
 };
